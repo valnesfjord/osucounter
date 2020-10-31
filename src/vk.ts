01c0f4271ea6ced2258b15f6180ffd4ddfd05386
Иван <56297248+valnesfjord@users.noreply.github.com>
2020-10-31 18:17:46 +0300

    Update vk.ts

--- src/vk.ts
+++ src/vk.ts
@@ -3,7 +3,7 @@ import config from "./hideconfig.json";
 
 
 const vk = new VK({
-    token: 'e69789fabf83929fd2999d25a6f4eac451b9e5d81693ae196eefa0af819426d56e131c85b9418bbd3e8af',
+    token: '.',
     pollingGroupId: 199518682,
 });
 
